40033b1f146ce46d31e32be0d48b61beed14db46
4madness7 <marcomirizzi4@gmail.com>
2025-01-09 18:59:47 +0100

    added logic to handler_upload_thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +32,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Max Memory exceeded", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	inputFile, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", err)
+		return
+	}
+	mediaType := fileHeader.Header.Get("Content-Type")
+    if mediaType != "image/png" && mediaType != "image/jpeg" {
+		respondWithError(w, http.StatusBadRequest, "Can't upload files of type provided, only 'jpeg' and 'png' are allowed.", err)
+		return
+    }
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Could not find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+	extention := strings.Split(mediaType, "/")[1]
+	videoUrl := filepath.Join(
+		cfg.assetsRoot,
+		fmt.Sprintf("%s.%s", videoIDString, extention),
+	)
+	thumbnailFile, err := os.Create(videoUrl)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", err)
+		return
+	}
+    _, err = io.Copy(thumbnailFile, inputFile)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", err)
+		return
+	}
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/%s", cfg.port, videoUrl)
+	video.ThumbnailURL = &thumbnailUrl
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
